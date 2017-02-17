a728aa089c6d90ebe913fd296ba3de732c4c6042
Mitchell Caisse <computers1055@gmail.com>
2017-02-16 19:41:10 -0500

    Added a basic class counter implementation

---
+++ src/ClassCounter/ClassCounter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassCounter
+{
+    public class ClassCounter
+    {
+
+        /// <summary>
+        /// Mapping of number of times a given type has been created / instantiated  
+        /// </summary>
+        private Dictionary<Type, int> Creations { get; }
+
+        /// <summary>
+        /// Mapping of number of times a given type has been removed / freed by garbage collection
+        /// </summary>
+        private Dictionary<Type, int> Removals { get; }
+
+        public ClassCounter()
+        {
+            Creations = new Dictionary<Type, int>();
+            Removals = new Dictionary<Type, int>();
+        }
+
+        /// <summary>
+        /// Increments the number of times the given type has been created
+        /// </summary>
+        /// <param name="type">The type</param>
+        public void ClassCreated(Type type)
+        {
+            if (!Creations.ContainsKey(type))
+            {
+                Creations.Add(type, 0);
+            }
+
+            Creations[type] = Creations[type] + 1;
+
+        }
+
+        /// <summary>
+        /// Increments the number of times the type of the given object has been created
+        /// </summary>
+        /// <param name="o">the object</param>
+        public void ClassCreated(Object o)
+        {
+            ClassCreated(o.GetType());
+        }
+
+
+        /// <summary>
+        /// Increments the number of times the the given type has been removed
+        /// </summary>
+        /// <param name="type">The type</param>
+        public void ClassRemoved(Type type)
+        {
+            if (!Removals.ContainsKey(type))
+            {
+                Removals.Add(type, 0);
+            }
+
+            Removals[type] = Removals[type] + 1;
+        }
+
+        /// <summary>
+        /// Increments the number of times the type of the given object has been removed
+        /// </summary>
+        /// <param name="o">the object</param>
+        public void ClassRemoved(Object o)
+        {
+            ClassRemoved(o.GetType());
+        }
+
+    }
+}
