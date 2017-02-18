d1d55f7bd7f29f54314d29d4274fa2d57dedaae0
Mitchell Caisse <computers1055@gmail.com>
2017-02-17 22:57:34 -0500

    Added some comments

--- src/ClassCounter/ClassCounterBase.cs
+++ src/ClassCounter/ClassCounterBase.cs
@@ -5,14 +5,25 @@ using System.Threading.Tasks;
 
 namespace ClassCounter
 {
+
+    /// <summary>
+    /// Base class to automatically add the Counting + Removal calls to a class
+    /// </summary>
     public abstract class ClassCounterBase
     {
 
+
+        /// <summary>
+        /// When a class is created we want to increment the counter
+        /// </summary>
         protected ClassCounterBase()
         {
             ClassCounter.ClassCreated(this);
         }
 
+        /// <summary>
+        /// When a class is removed, we want to increment the removal counter.
+        /// </summary>
         ~ClassCounterBase()
         {
             ClassCounter.ClassRemoved(this);
